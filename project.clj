772adfe399c613a647fb09d4bb1e9367f765bb23
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-02-10 13:04:06 +0100

    add basic hello-world GET endpoints

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject clojure-rest-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure experimenting with restful api"
+  :url ""
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"] ; Routing library
+                 [http-kit "2.3.0"] ; Http library for client/server
+                 [ring/ring-defaults "0.3.2"] ; Query params etc
+                 [org.clojure/data.json "0.2.6"]] ; Clojure JSON library
   :main ^:skip-aot clojure-rest-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojure_rest_api/core.clj
+++ src/clojure_rest_api/core.clj
@@ -1,7 +1,37 @@
 (ns clojure-rest-api.core
-  (:gen-class))
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json])
+  (:gen-class)) <
+
+(defn simple-body-page [reg]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello, World!"})
+
+(defn request-example [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (->>
+          (pp/pprint req)
+          (str "Request Object: " req))})
+
+(defroutes app-routes
+  (GET "/" [] simple-body-page)
+  (GET "/request" [] request-example)
+  (route/not-found "Error, page not found!"))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Main entry point"
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ; run server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run server without ring defaults
+    ; (server/run-server #'app-routes {:port port}) 
+    (println (str "Running webserver at http://127.0.1.1:" port "/"))))
+
