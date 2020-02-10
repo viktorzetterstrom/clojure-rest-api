9f52b50fbd0e5e00da4e5ae4bbe420297d5121be
Viktor Zetterström <viktor.zetterstrom@footway.com>
2020-02-10 08:29:45 +0100

    initial commit, leiningen setup

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-rest-api
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojure_rest_api/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-rest-api.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
